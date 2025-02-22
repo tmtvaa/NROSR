9fb5e5438fd0dde4d3db5eed89ed840bc8ac1aa2
tmtvaa <tmtvaa@gmail.com>
2025-02-22 11:11:47 +0100

    Add files via upload

---
+++ metrics.sh
@@ -0,0 +1,6 @@
+python PSNR_SSIM_MSE_LPIPS_DISTS.py ./datasets_for_test/BSD100_SR/images ./results_ours_BSD100/pred_hr_enh ./results_ours_BSD100/PSNR_SSIM_MSE_LPIPS_DISTS.csv cpu
+python PSNR_SSIM_MSE_LPIPS_DISTS.py ./datasets_for_test/Set5_SR/images ./results_ours_Set5/pred_hr_enh ./results_ours_Set5/PSNR_SSIM_MSE_LPIPS_DISTS.csv cpu
+python PSNR_SSIM_MSE_LPIPS_DISTS.py ./datasets_for_test/Set14_SR/images ./results_ours_Set14/pred_hr_enh ./results_ours_Set14/PSNR_SSIM_MSE_LPIPS_DISTS.csv cpu
+python PSNR_SSIM_MSE_LPIPS_DISTS.py ./datasets_for_test/Urban100_SR/images ./results_ours_Urban100/pred_hr_enh ./results_ours_Urban100/PSNR_SSIM_MSE_LPIPS_DISTS.csv cpu
+python PSNR_SSIM_MSE_LPIPS_DISTS.py ./datasets_for_test/Saffron/images ./results_ours_Saffron/pred_hr_enh ./results_ours_Saffron/Ours_PSNR_SSIM_MSE_LPIPS_DISTS.csv cpu
+
